53b68ba2241e6fe344ed62121cfdd3ecc1ac77e2
lucas-adb <alvesdebrito.lucas@gmail.com>
2025-11-05 14:50:06 -0300

    test: Adds all the good scenarios tests

--- TaskManager.Tests/TaskServiceTests.cs
+++ TaskManager.Tests/TaskServiceTests.cs
@@ -47,6 +47,38 @@ public class TaskServiceTests
     }
 
     [Fact]
+    public async Task GetAllAsync_should_return_all_existing_tasks()
+    {
+        var options = GetInMemoryOptions();
+
+        using (var context = new AppDbContext(options))
+        {
+            context.Tasks.Add(new TaskEntity
+            {
+                Title = "Task 1",
+                CreationDate = DateOnly.FromDateTime(DateTime.UtcNow)
+            });
+
+            context.Tasks.Add(new TaskEntity
+            {
+                Title = "Task 2",
+                CreationDate = DateOnly.FromDateTime(DateTime.UtcNow)
+            });
+            await context.SaveChangesAsync();
+        }
+
+        using (var context = new AppDbContext(options))
+        {
+            var repo = new TaskRepository(context);
+            var service = new TaskService(repo);
+            var dtoList = await service.GetAllAsync(1, 10);
+
+            Assert.Equal(2, dtoList.Count);
+            Assert.Collection(dtoList, item => Assert.Equal("Task 1", item.Title), item => Assert.Equal("Task 2", item.Title));
+        }
+    }
+
+    [Fact]
     public async Task GetByIdAsync_should_return_existing_task()
     {
         var options = GetInMemoryOptions();
@@ -55,7 +87,7 @@ public class TaskServiceTests
         {
             context.Tasks.Add(new TaskEntity
             {
-                Title = "Seed",
+                Title = "Task",
                 CreationDate = DateOnly.FromDateTime(DateTime.UtcNow)
             });
             await context.SaveChangesAsync();
@@ -68,7 +100,76 @@ public class TaskServiceTests
             var dto = await service.GetByIdAsync(1);
 
             Assert.NotNull(dto);
-            Assert.Equal("Seed", dto!.Title);
+            Assert.Equal("Task", dto!.Title);
+        }
+    }
+
+    [Fact]
+    public async Task RemoveByIdAsync_should_remove_existing_task()
+    {
+        var options = GetInMemoryOptions();
+
+        using (var context = new AppDbContext(options))
+        {
+            context.Tasks.Add(new TaskEntity
+            {
+                Title = "Task",
+                CreationDate = DateOnly.FromDateTime(DateTime.UtcNow)
+            });
+            await context.SaveChangesAsync();
+        }
+
+        using (var context = new AppDbContext(options))
+        {
+            var repo = new TaskRepository(context);
+            var service = new TaskService(repo);
+            var dto = await service.RemoveByIdAsync(1);
+
+            var count = await context.Tasks.CountAsync();
+            Assert.Equal(0, count);
+        }
+    }
+
+    [Fact]
+    public async Task UpdateAsync_should_update_task()
+    {
+        var options = GetInMemoryOptions();
+
+        using (var context = new AppDbContext(options))
+        {
+            var repo = new TaskRepository(context);
+            var service = new TaskService(repo);
+
+            var createDto = new TaskCreateDto
+            {
+                Title = "Teste",
+                Description = "Descrição",
+                Responsible = "Fulano"
+            };
+
+            await service.CreateAsync(createDto);
+        }
+
+        using (var context = new AppDbContext(options))
+        {
+            var repo = new TaskRepository(context);
+            var service = new TaskService(repo);
+
+
+            var updateDto = new TaskUpdateDto
+            {
+                Title = "Atualizado",
+                Description = "Atualizado",
+                CompletionDate = DateOnly.FromDateTime(DateTime.UtcNow),
+                Status = TaskStatusEnum.Concluida,
+                Responsible = "Atualizado"
+            };
+
+            var updated = await service.UpdateAsync(1, updateDto);
+
+            Assert.NotNull(updated);
+            Assert.Equal("Atualizado", updated.Title);
+            Assert.Equal("Atualizado", updated.Responsible);
         }
     }
 }
